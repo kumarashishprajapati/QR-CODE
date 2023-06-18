0a7404a5bcbb1cc74876ac8c292633bf987f2188
Ashish Kumar <116018434+kumarashishprajapati@users.noreply.github.com>
2023-06-18 15:47:23 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,16 @@
+#  01  without color QRcode
+
+# import qrcode as qr
+# img =qr.make("https://www.youtube.com/channel/UCrqpJRz037P__tyAf78irxw")
+# img.save("AK Invisible.png")
+
+#  02  with color Qr code
+
+import qrcode 
+from PIL import Image
+
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=10)
+qr.add_data("https://www.youtube.com/channel/UCrqpJRz037P__tyAf78irxw")
+qr.make(fit=True)
+img=qr.make_image(fill_color="green",back_color="white")
+img.save("Color QR ")
